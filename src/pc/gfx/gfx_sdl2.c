bc905d9f6f643762fab7516c75be16f41b4436b4
flatmapthatshit <48256950+flatmapthatshit@users.noreply.github.com>
2025-07-16 13:19:07 +0100

    feat(SDL): Enable high DPI support and fix window size

--- src/pc/gfx/gfx_sdl2.c
+++ src/pc/gfx/gfx_sdl2.c
@@ -242,7 +242,7 @@ static void gfx_sdl_init(const char *window_title) {
     wnd = SDL_CreateWindow(
         window_title,
         xpos, ypos, configWindow.w, configWindow.h,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+        SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
     ctx = SDL_GL_CreateContext(wnd);
 
@@ -270,7 +270,8 @@ static void gfx_sdl_main_loop(void (*run_one_game_iter)(void)) {
 
 static void gfx_sdl_get_dimensions(uint32_t *width, uint32_t *height) {
     int w, h;
-    SDL_GetWindowSize(wnd, &w, &h);
+    SDL_GL_GetDrawableSize(wnd, &w, &h);
+
     if (width) *width = w;
     if (height) *height = h;
 }
